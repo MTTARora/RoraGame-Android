1571d1746b3f2d47d5d915cb021607dc40bd8149
Anh Mai <mtta200396@gmail.com>
2018-12-29 10:59:47 +0700

    Changing files

---
+++ RoraGame/moonlight-common 10.54.18 AM/src/main/java/com/roragame/nvstream/av/audio/AudioRenderer.java
@@ -0,0 +1,13 @@
+package com.roragame.nvstream.av.audio;
+
+public interface AudioRenderer {
+	int setup(int audioConfiguration);
+
+	void start();
+
+	void stop();
+	
+	void playDecodedAudio(byte[] audioData);
+	
+	void cleanup();
+}
